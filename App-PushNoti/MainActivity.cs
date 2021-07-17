189bedbf87e6f7c17edba730a697ebaa7ad04580
Mahdi khayamdar <48985789+Mahdikh75@users.noreply.github.com>
2021-07-17 13:00:39 +0430

    Initial commit

---
+++ App-PushNoti/MainActivity.cs
@@ -0,0 +1,155 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Support.V4.App;
+
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+using Android.Gms.Common;
+
+namespace App_PushNoti
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button Btn_Token;TextView msgText;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            msgText = (TextView)FindViewById(Resource.Id.textView1);
+            Btn_Token = (Button)FindViewById(Resource.Id.button1);
+
+            Btn_Token.Click += delegate
+            {
+                msgText.Text = "InstanceID token: " + FirebaseInstanceId.Instance.Token.ToString();
+                Xamarin.Essentials.Share.RequestAsync(FirebaseInstanceId.Instance.Token);
+            };
+
+            IsPlayServicesAvailable();
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                }
+                else
+                {
+                    msgText.Text = "This device is not supported";
+                    //Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    // get id token
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+        void SendRegistrationToServer(string token)
+        {
+            Toast.MakeText(this, token, ToastLength.Short).Show();
+        }
+    }
+
+    // get notications
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+
+            var body = message.GetNotification().Body;
+            var title = message.GetNotification().Title;
+
+            Log.Debug(TAG, "Notification Message Body: " + body);
+           
+            SendNotification(body, title);
+        }
+
+        void SendNotification(string messageBody, string Title)
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            const int pendingIntentId = 0;
+            PendingIntent pendingIntent = PendingIntent.GetActivity(this, pendingIntentId, intent, PendingIntentFlags.OneShot);
+          
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my_notification_channel")
+                .SetContentIntent(pendingIntent)
+                .SetContentTitle(Title)
+                .SetContentText(messageBody)
+                .SetVibrate(new long[] { 120, 50 })
+                .SetAutoCancel(false)
+                .SetSmallIcon(Resource.Mipmap.ic_launcher);
+
+            Notification notification = builder.Build();
+            NotificationManager notificationManager = GetSystemService(Context.NotificationService) as NotificationManager;
+
+            notificationManager.Notify(100, notification);
+        }
+
+    }
+
+}
+
+
+/*
+ * 
+ *  <receiver
+    android:name="com.google.firebase.iid.FirebaseInstanceIdInternalReceiver"
+    android:exported="false" />
+
+    <receiver
+
+        android:name="com.google.firebase.iid.FirebaseInstanceIdReceiver"
+        android:exported="true"
+        android:permission="com.google.android.c2dm.permission.SEND">
+
+      <intent-filter>
+        <action android:name="com.google.android.c2dm.intent.RECEIVE" />
+        <action android:name="com.google.android.c2dm.intent.REGISTRATION" />
+        <category android:name="${applicationId}" />
+      </intent-filter>
+
+    </receiver>
+ * 
+ * 
+ * 
+ * */
